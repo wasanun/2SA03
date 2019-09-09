8378c89edd5de1a78e3c21400f9ea5bf9051e0ec
wasanun <wasanun_04@hotmail.com>
2019-09-09 09:32:06 +0700

    Handler as Props

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -10,9 +10,11 @@ export default class CharacterCard extends Component {
    
     activate = () => {
         if(!this.state.active){
-            this.setState({active: true})
+        this.props.activationHandler(this.props.value)
+        this.setState({active: true})
         }
-    }
+       }
+       
     render(){
         let className = `card ${this.state.active ? 'activeCard': ''}`
     return (
--- src/WordCard.js
+++ src/WordCard.js
@@ -4,10 +4,13 @@ import CharacterCard from './CharacterCard.js';
 import _ from 'lodash';
 
 export default class WordCard extends Component {
+    activationHandler = c => { 
+        console.log(`${c} has been activated.`) 
+    }
     render() {
     return (
         <div>
-            { Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i}/>) }
+            { Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i} activationHandler={this.activationHandler}/>) }
         </div>
     );
     }
