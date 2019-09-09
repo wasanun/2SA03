0ac4893898b385fa8f11885b821725e746744d86
wasanun <wasanun_04@hotmail.com>
2019-09-09 08:39:47 +0700

    secon

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CharacterCard from './CharacterCard';
 
-function App() {
+
+const word = "Hello";
+class App extends Component {
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+  {
+    Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+  }
+  </div>
   );
+ }
 }
 
 export default App;
