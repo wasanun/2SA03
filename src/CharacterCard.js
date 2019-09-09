f04d25a52c7653b67fd6ecc1840629e9be2e63f2
wasanun <wasanun_04@hotmail.com>
2019-09-09 09:46:37 +0700

    Game Logic

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,27 +1,31 @@
 import React, { Component } from 'react';
+import './App.css';
 
 export default class CharacterCard extends Component {
-    constructor(props){
-    super(props)
-    this.state = {   
-         active: false,
+    constructor(props) {
+        super(props)
+        this.state = {
+            active: false,
+        }
     }
-    }
-   
+
     activate = () => {
-        if(!this.state.active){
-        this.props.activationHandler(this.props.value)
-        this.setState({active: true})
+        if (!this.state.active) {
+            this.props.activationHandler(this.props.value)
+            this.setState({ active: true })
+        }
+    }
+    componentDidUpdate(prevProps){
+        if(prevProps.attempt != this.props.attempt){
+        this.setState({active: false})
         }
        }
-       
-    render(){
-        let className = `card ${this.state.active ? 'activeCard': ''}`
-    return (
-        <div className={className} onClick={this.activate}>
-            {this.props.value}
-        </div>
-    )
+    render() {
+        let className = `card ${this.state.active ? 'activeCard' : ''}`
+        return (
+            <div className={className} onClick={this.activate}>
+                {this.props.value}
+            </div>
+        )
     }
-   }
-
+}
--- src/WordCard.js
+++ src/WordCard.js
@@ -2,16 +2,54 @@ import React, { Component } from 'react';
 import './App.css';
 import CharacterCard from './CharacterCard.js';
 import _ from 'lodash';
+const prepareStateFromWord = (given_word) => {
+    let word = given_word.toUpperCase()
+    let chars = _.shuffle(Array.from(word))
+    return {
+        word,
+        chars,
+        attempt: 1,
+        guess: [],
+        completed: false
+    }
+}
 
-export default class WordCard extends Component {
-    activationHandler = c => { 
-        console.log(`${c} has been activated.`) 
+export default class WordCard extends
+    Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            attempt: '',
+        }
+    }
+    componentWillMount(){
+        let data = prepareStateFromWord(this.props.value);
+        this.setState({
+            word:data.word,
+            chars: data.chars,
+            attempt: data.attempt,
+            guess: data.guess,
+            completed: data.completed,
+        })
+    }
+    activationHandler = (c) => {
+        let guess = [...this.state.guess, c]
+        this.setState({ guess })
+        if (guess.length == this.state.chars.length) {
+            if (guess.join('').toString() == this.state.word) {
+                this.setState({ guess: [], completed: true })
+            } else {
+                this.setState({ guess: [], attempt: this.state.attempt + 1 })
+            }
+        }
     }
     render() {
-    return (
-        <div>
-            { Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i} activationHandler={this.activationHandler}/>) }
-        </div>
-    );
+        console.log(this.state);
+        return (
+            <div>
+                {this.state.chars.map((c, i) => <CharacterCard value={c} key={i} attempt={this.state.attempt}
+                    activationHandler={this.activationHandler} />)}
+            </div>
+        );
     }
 }
